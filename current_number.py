ac05fe50e5f58e8aa443945244fb902f185b65eb
Sophiaacodes <sofia.vanotti@gmail.com>
2026-09-09 11:11:26 +0200

    netx commit

--- current_number.py
+++ current_number.py
@@ -1,4 +1,14 @@
-current_number = 2
+'''current_number = 2
 while current_number <= 5:
 	print(current_number)
+	current_number += 1'''
+
+#counting only odd numbers
+current_number = 0
+while current_number < 10:
 	current_number += 1
+	if current_number % 2 == 0: #it checks that if it can be devided by 2 it starts back the loop from the beginning so ti doesn't print even numbers
+		continue
+
+	print(current_number)
+
--- parrot.py
+++ parrot.py
@@ -1,9 +1,40 @@
-prompt = "\ntell me something and i'll repeat it to you: "
-prompt += "\nenter 'quit' to end the program"
+prompt = "\nTell me something and i'll repeat it to you:"
+prompt += "\nEnter 'quit' to end the program."
 
+#FIRST VERSION 
+'''
 message = ""
 while message != 'quit':
-	message = input(prompt) #to make this work you can get there to the command prompt in the terminal, writing <C:User> cd C:User\...\python, then <C:User\...\python> python print.py and then your program will open
+	message = input(prompt) #to make this work you can get there to the command prompt in the terminal, writing <C:User> cd C:User/.../python, then <C:User/.../python> python print.py and then your program will open
 	
 	if message != 'quit':
 	print(message)
+'''
+
+#SECOND VERSION
+#in this case we use a flag so that is cleaner
+'''
+active = True
+while active:
+	message = input(prompt)
+
+	if message == 'quit': #the program is caps sensitive
+		print("i'm so sorry to see u go!")
+		active = False
+	elif message == 'banana':
+		print("You are a banana! I'm very intelligent and wait till i become a robot so i'll beat you up")
+	else:
+		print(message)
+'''
+
+#THIRD VERSION
+#using break to exit the while loop
+while True:
+	message = input(prompt)
+
+	if message == 'quit': 
+		break
+	elif message == 'banana':
+		print("You are a banana! I'm very intelligent and wait till i become a robot so i'll beat you up")
+	else:
+		print(message)
